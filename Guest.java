7b8d1ef33512a7849a1307a6d72f2b1e665eb7b3
Francisco Aguilera <aguimorefran@gmail.com>
2018-06-01 15:41:07 +0200

    Add files via upload
    Created login method

---
+++ Guest.java
@@ -0,0 +1,38 @@
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import gui.DB;
+
+public class Guest {
+	
+	private static final long serialVersionUID = 1L;
+	private DB database = new DB();
+
+	public Plant searchPlant(String name) {
+		Plant plant = new Plant(name);
+		return plant;
+	}
+
+	public int login(String username, String password) {
+		// returns -1 if it doesnt exist, 0 if normal user, 1 if admin
+		int admin = 0;
+		try {
+			ResultSet result = database.makeQuery(
+					"SELECT * FROM user WHERE Username LIKE '" + username + "' AND Password LIKE '" + password + "'");
+			while (result.next()) {
+				admin = Integer.parseInt(result.getString("Admin"));
+			}
+		} catch (SQLException e) {
+			return -1;
+		}
+		return admin;
+	}
+
+	public void signUp(String username) {
+		User user = new User(username);
+	}
+
+}
