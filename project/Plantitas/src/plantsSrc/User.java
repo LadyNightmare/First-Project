b463606a299a979c9daad6cfe84848de072ef324
LadyNightmare <ladynightmare2@outlook.com>
2018-06-02 00:00:04 +0200

    Update User.java
    User updated but not yet finished

--- project/Plantitas/src/plantsSrc/User.java
+++ project/Plantitas/src/plantsSrc/User.java
@@ -4,11 +4,40 @@ public class User {
 
     private String username;
     private String password;
+    private String email;
+    private boolean logged;
+    public static DB database = new DB();
 
-    public User(String username) {
+    public User(String username, String password, String email) {
 
         this.username = username;
+        this.password = password;
+        this.email = email;	
+        logged = true;
 
     }
+    
+    public void logout() {
+    	
+    	logged = false;
+    	
+    }
+    
+    public void writePost (Post post) {
+    	
+    	
+    	
+    }
+    
+    public void addComment (Comment comment) {
+    	
+    	database.addComment(comment.)
+    	
+    }
+    
+    public Plant searchPlant(String name) {
+		Plant plant = new Plant(name);
+		return plant;
+	}
 
 }
